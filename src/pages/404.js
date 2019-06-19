9aa695438fcf0a6ea4d668fd112c8ef41cb164b6
77ccreed <kiur.jullis@gmail.com>
2019-06-19 14:17:10 +0300

    404 leht eesti keelseks

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,8 @@ import Layout from '../components/Layout'
 const NotFoundPage = () => (
   <Layout>
     <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>Lehte ei leitud!</h1>
+      <p>Palun pöördu tagasi pealehele.</p>
     </div>
   </Layout>
 )
