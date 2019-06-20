5d0392aaea19fd0615688c45e63699016a1b78b0
77ccreed <kiur.jullis@gmail.com>
2019-06-20 08:18:45 +0300

    titel ja desc

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'ESC Grupp OÜ',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Kasutatud autode müük Valgas. Kõik meie autod on läbinud põhjaliku tehnilise taustakontrolli ja müügieelse ülevaatuse. Sõidukite tellimine ja trantsport Euroopast. Komisjonimüük kodulehel ja Valga autoplatsil aadressil Viljandi mnt 25. Abistame autoostjat liisingu, laenu ja dokumentide vormistamisel. Lühi- ja pikaajaline autorent.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
