92de334526f26b2cd8ae1767b7f7d7a4c0a287c5
77ccreed <kiur.jullis@gmail.com>
2019-06-22 07:54:01 +0300

    navbar logo asendatud

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +41,11 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <p style={{ 
+                color: '#ff4400',
+                fontSize:'1.2rem',
+                fontWeight: '600'
+            }}>ESC Grupp OÜ</p>
             </Link>
             {/* Hamburger menu */}
             <div
