0525af9765e4dae54dc29c61fa4b5c2de7824cea
77ccreed <kiur.jullis@gmail.com>
2019-07-19 10:21:23 +0300

    example ja file upload

--- src/pages/kontaktid/examples.js
+++ src/pages/kontaktid/examples.js
@@ -16,10 +16,10 @@ export default class Index extends React.Component {
               </p>
               <ul>
                 <li>
-                  <Link to="/contact">Basic contact form</Link>
+                  <Link to="/kontaktid">Basic contact form</Link>
                 </li>
                 <li>
-                  <Link to="/contact/file-upload/">Form with file upload</Link>
+                  <Link to="/kontaktid/file-upload/">Form with file upload</Link>
                 </li>
               </ul>
 
