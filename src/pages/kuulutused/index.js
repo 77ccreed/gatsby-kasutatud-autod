1675e32936e04360ff87e24bcedf190ae11b69b9
77ccreed <kiur.jullis@gmail.com>
2020-04-13 08:25:19 +0300

    taustapilt

--- src/pages/kuulutused/index.js
+++ src/pages/kuulutused/index.js
@@ -15,7 +15,7 @@ export default class BlogIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/kasutatud-autod-valgas.jpg')`,
+            backgroundImage: `url('/img/kasutatud-autod.jpg')`,
           }}
         >
           <h1
