c86c78ca056b113cf50f7561bb079319de29a0f1
Kiur Jullis <kiur.jullis@gmail.com>
2022-06-26 07:50:04 +0300

    järelmaks

--- src/pages/liising/index.js
+++ src/pages/liising/index.js
@@ -15,13 +15,56 @@ const Järelmaks = () => {
             <div className="column is-10 is-offset-1">
               <div className="section">
                 <h1 className="title is-size-3 has-text-weight-bold is-bold-light">Järelmaksu tingimused</h1>
-                <p className="content">Oled vähemalt 21-aastane Eesti kodanik või Sul on elamisluba.</p>
-                <p className="content">Sinu igakuine netosissetulek on vähemalt 280 eurot.</p>
-                <p className="content">Sa täidad oma rahalisi kohustusi korrektselt.</p>
-                <p className="content">Järelmaksusumma, mida saame Sinule pakkuda, sõltub Sinu igakuisest sissetulekust ja kohustustest.</p>
+                <ul className="content">
+                  <li>
+                    <p>
+                      ✔️ Oled vähemalt 21-aastane Eesti kodanik või Sul on elamisluba.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      ✔️ Sinu igakuine netosissetulek on vähemalt 280 eurot.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      ✔️ Sa täidad oma rahalisi kohustusi korrektselt.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      ✔️ Järelmaksusumma, mida saame Sinule pakkuda, sõltub Sinu igakuisest sissetulekust ja kohustustest.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      ✔️ Laenu summa kuni 25 000 EUR
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      ✔️ Intress alates 9,9% aastas - laenujäägilt
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      ✔️ Sissemakse 0%
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      ✔️ Enneaege tagastamine TASUTA
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      ✔️ Põhiosa vähendamine TASUTA
+                    </p>
+                  </li>
+                </ul>
                 <hr />
-                <a className="is-size-4 btn" href="https://docs.google.com/forms/d/e/1FAIpQLSeBQV7MpfVycfktm-GZIOH-iwO3cUYyPPYqYDuIwK9euqlM9Q/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer"
-                  title="Järelmaks">Järelmaksu taotle siin.</a>
+                <a className="is-size-4 btn" href="https://www.primero.ee/autolaen/auth?promo=valgaautoaed" target="_blank" rel="noopener noreferrer"
+                  title="Järelmaks">Taotluse esitamist alusta siit.</a>
                 <hr />
                 <h2 className="title is-size-4 has-text-weight-bold is-bold-light">Osamaksete tasumine</h2>
                 <p className="content">Saadame Sinu kodupanga internetipanka igal kuul e-arve. Osamaksete tasumise tähtaeg on iga kuu 12. kuupäev.</p>
@@ -44,8 +87,8 @@ const Järelmaks = () => {
                 <p className="content">Kui oled lepingu sõlminud, vaata veelkord üle tingimused. Nii saad olla kindel, et täidad oma kohustused õigel viisil ja õigel ajal.</p>
                 <p className="content">Tutvu meie kirjadega tähelepanelikult ning anna meile kohe teada, kui Sinu kontaktandmed muutuvad.</p>
                 <hr />
-                <a className="is-size-4 btn" href="https://docs.google.com/forms/d/e/1FAIpQLSeBQV7MpfVycfktm-GZIOH-iwO3cUYyPPYqYDuIwK9euqlM9Q/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer"
-                  title="Järelmaks">Järelmaksu taotle siin.</a>
+                <a className="is-size-4 btn" href="https://www.primero.ee/autolaen/auth?promo=valgaautoaed" target="_blank" rel="noopener noreferrer"
+                  title="Järelmaks">Taotluse esitamist alusta siit.</a>
               </div>
             </div>
           </div>
